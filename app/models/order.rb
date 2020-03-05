3a5bf5634237bedb8b5c3de787cd8286e8301b66
Eden Yang <dexuan90213@gmail.com>
2020-03-05 11:09:50 +0800

    add Order CRUD and AASM state

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,19 @@
 class Order < ApplicationRecord
+  include AASM
+  acts_as_paranoid
+
   belongs_to :customer
 
   validates :number, :delivered_at, presence: true
   validates :number, length: { is: 6 }, uniqueness: true
+
+  aasm no_direct_assignment: true do
+    state :pending, initial: true
+    state :delivered
+    state :wait_for_payment, :paid, :closed
+
+    event :deliver do
+      transitions from: :pending, to: :delivered
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root 'orders#index'
+
   resources :areas, except: [:show]
   resources :customers, except: [:show]
   resources :products, except: [:show]
